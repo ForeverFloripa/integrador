c450f56d2bcf5e7fa69e66cd031a7848ed188cea
ForeverFloripa <34176505+ForeverFloripa@users.noreply.github.com>
2020-06-08 19:25:36 -0300

    commit recebendo produtos corrigir float

--- integrador/src/model/Produto.java
+++ integrador/src/model/Produto.java
@@ -1,38 +1,44 @@
 package model;
 
-import java.math.BigDecimal;
-
 public class Produto {
-	
+
 	private String codigo;
 	private String descricao;
 	private String grupo;
-	private BigDecimal preco;
-	
+	private Float preco;
+
 	public String getCodigo() {
 		return codigo;
 	}
+
 	public void setCodigo(String codigo) {
 		this.codigo = codigo;
 	}
+
 	public String getDescricao() {
 		return descricao;
 	}
+
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
+
 	public String getGrupo() {
 		return grupo;
 	}
+
 	public void setGrupo(String grupo) {
 		this.grupo = grupo;
 	}
-	public BigDecimal getPreco() {
+
+	public float getPreco() {
 		return preco;
 	}
-	public void setPreco(BigDecimal preco) {
+
+	public void setPreco(float preco) {
 		this.preco = preco;
 	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -40,6 +46,7 @@ public class Produto {
 		result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
 		return result;
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -56,18 +63,27 @@ public class Produto {
 			return false;
 		return true;
 	}
-	public Produto(String codigo, String descricao, String grupo, BigDecimal preco) {
-		super();
-		this.codigo = codigo;
-		this.descricao = descricao;
-		this.grupo = grupo;
-		this.preco = preco;
-	}
-	
-	
-	
-	
-	
-	
 
+	public Produto() {
+	}
+
+	public Produto(String line) {
+		if (null != line) {
+			String[] valores = line.split(";");
+			if (null != valores) {
+				setCodigo(valores[0]);
+				setDescricao(valores[1]);
+				setGrupo(valores[2]);
+				setPreco(Float.valueOf(valores[3]));
+			}
+		}
+	}
+/*
+	@Override
+	public String toString() {
+		return "Produto [codigo=" + codigo + ", descricao=" + descricao + ", grupo=" + grupo + ", preco=" + preco + "]";
+	}
+*/
+	
+	
 }
--- integrador/src/servico/Recebe.java
+++ integrador/src/servico/Recebe.java
@@ -5,55 +5,80 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Produto;
 
 public class Recebe {
+	
+    public static final String SEPARADOR = ";";
+    public static final String ARQUIVO = "C:\\integracao\\recebe\\produtoslista.CSV";
 
 	public static void main(String[] args) throws IOException {
-		lerArquivo();
-		
-		splitando();
-	
-	}
-		
-		
-	public static void lerArquivo() throws IOException {	
-		
-		
-		
-		System.out.println("Iniciando Importação");
-		String nomeArquivo= "C:\\integracao\\recebe\\produtoslista.CSV";
-		File arq = new File(nomeArquivo);
-		
-		
-		
-		if(arq.exists()) {
-			System.out.println("arquivo existe");
-		
-			FileReader fr = new FileReader(nomeArquivo);
-            BufferedReader br = new BufferedReader(fr);
-            String s = null;
-            while ((s = br.readLine()) != null) {
-            	System.out.println(s);
+			//Produto produto = new Produto();
+			
+			//splitando();
+		try {
+
+            List<Produto> produtos = carregaProduto();
+
+            for(final Produto p : produtos){
+                System.out.println(p.getCodigo()+", "+p.getDescricao()+", "+p.getGrupo()+", "+p.getPreco());
             }
-            }
-		}
-	
-	public static void splitando() throws IOException {
-        File file = new File("C:\\integracao\\recebe\\produtoslista.CSV");
-        String texto = new String(Files.readAllBytes(file.toPath()));
-        String[] splitados = texto.split(";");
-        for (String col : splitados) {
-            
-            System.out.print(col+ " ");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
-    }
-	
-	
-	
-	
+		
+		
+	}
+
+	public static List<Produto> carregaProduto() throws IOException {
+		List<Produto> list = new ArrayList<Produto>(0);
+		FileReader arq = new FileReader(ARQUIVO);
+		BufferedReader lerArq = new BufferedReader(arq);
+		String linha = lerArq.readLine();
+		while (linha != null) {
+			System.out.printf("%s\n", linha);
+			linha = lerArq.readLine();
+			if(null != linha && !"".equals(linha) ){
+				Produto produto = new Produto(linha);
+				list.add(produto);
+			}
+		}
+
+		return list;
+	}
+
+	public static void lerArquivo() throws IOException {
+
+		System.out.println("Iniciando Importação");
+		String nomeArquivo = "C:\\integracao\\recebe\\produtoslista.CSV";
+		File arq = new File(nomeArquivo);
+
+		if (arq.exists()) {
+			System.out.println("arquivo existe");
+
+			FileReader fr = new FileReader(nomeArquivo);
+			BufferedReader br = new BufferedReader(fr);
+			String s = null;
+			while ((s = br.readLine()) != null) {
+				System.out.println(s);
+			}
+			br.close();
+		}
+	}
+
+	public static void splitando() throws IOException {
+		File file = new File("C:\\integracao\\recebe\\produtoslista.CSV");
+		String texto = new String(Files.readAllBytes(file.toPath()));
+		String[] splitados = texto.split(SEPARADOR);
+		for (String col : splitados) {
+
+			System.out.print(col + " ");
+		}
+
+	}
+
 }
-
-
-
-
